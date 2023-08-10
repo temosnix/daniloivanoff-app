befacc67c7343592cb275f66509185dee79c739c
temosnix <temosnix@hotmail.com>
2023-08-10 13:30:00 -0300

    teste pagian de confirmaçao de notificação

--- testeapp/views.py
+++ testeapp/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
-from dotenv import load_dotenv
 import os
-from django.http import HttpResponse
+import json
 import requests
+from dotenv import load_dotenv
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+
+
 
 
 def notifications (request):
-    return render (request, 'notifications.html')
+   if request.method == 'POST':
+      data = json.loads(request.body)
+
+      _id = data.get('_id')
+      resource = data.get('resource')
+      user_id = data.get('user_id')
+      topic = data.get('topic')
+      application_id = data.get('application_id')
+      attempts = data.get('attempts')
+      sent = data.get('sent')
+      received = data.get('received')
+
+        # Exemplo de processamento (imprimindo os valores)
+      print("_id:", _id)
+      print("resource:", resource)
+      print("user_id:", user_id)
+      print("topic:", topic)
+      print("application_id:", application_id)
+      print("attempts:", attempts)
+      print("sent:", sent)
+      print("received:", received)
+
+      response_data = {'message': 'Requisição POST processada com sucesso'}
+      return JsonResponse(response_data, status=200)
+ 
 
 def index(request):
    load_dotenv()
