3438c415d8d60b783aa90a23c3076df9ae14b05f
temosnix <temosnix@hotmail.com>
2023-08-10 11:16:26 -0300

    primeiro teste notificaçoes.html

--- testeapp/views.py
+++ testeapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from dotenv import load_dotenv
 import os
 from django.http import HttpResponse
+import requests
 
 
 
@@ -13,18 +14,43 @@ def index(request):
    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
    REDIRECT_URI = os.getenv("REDIRECT_URI")
 
-   if CLIENT_ID == "3610569796440259":
-      texto = "3610569796440259"
-      return HttpResponse('certo')
-   else:
-      return HttpResponse('errado')
+   
+   return render(request, 'index.html')
    
 def uri (request):
    return render(request, 'uri.html')
 
 
 def notification(request):
-   return render(request, 'notification.html')
+
+    try:
+        #access_token = request.META.get('HTTP_ACCESS_TOKEN')
+        access_token = 'APP_USR-2417001236894079-081010-aaec5d1c2406664ec45fb0867b1b0058-34977269'
+        #meli_object = MeliObject(access_token)
+
+
+        user_response = requests.get('https://api.mercadolibre.com/users/me', headers={'Authorization': f'Bearer {access_token}'})
+        user = user_response.json()
+        
+        items_response = requests.get(f'https://api.mercadolibre.com/users/{user["id"]}/items/search', headers={'Authorization': f'Bearer {access_token}'})
+        items = items_response.json().get('results', [])
+        
+        result = []
+        for item_id in items:
+            item_response = requests.get(f'https://api.mercadolibre.com/items/{item_id}', headers={'Authorization': f'Bearer {access_token}'})
+            result.append(item_response.json())
+        
+        if result:
+            return render(request, 'notification.html', {'items': result})
+        else:
+            return HttpResponse('No items were found :(', status=404)
+        
+    except Exception as err:
+        print('Something went wrong', err)
+        return HttpResponse(f'Error! {err}', status=500)
+
+
+
 
    
 
